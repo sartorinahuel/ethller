294300c62f19522f391a5b58e7cd745ced66f1d0
Nahuel <nsartori@openpass.com.ar>
2021-01-31 17:41:04 -0300

    Update donations_page.dart

--- lib/pages/settings/donations/donations_page.dart
+++ lib/pages/settings/donations/donations_page.dart
@@ -19,65 +19,67 @@ class DonationsPage extends StatelessWidget {
             ),
           ),
         ),
-        body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          padding: const EdgeInsets.all(15),
-          child: ListView(
-            physics: BouncingScrollPhysics(),
-            children: [
-              Text(
-                'if you like it, also you can donate with what you think es fair.\n\nThanks and good mining!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  wordSpacing: 5,
-                  height: 2,
+        body: Builder(
+          builder: (BuildContext context) => Container(
+            height: double.infinity,
+            width: double.infinity,
+            padding: const EdgeInsets.all(15),
+            child: ListView(
+              physics: BouncingScrollPhysics(),
+              children: [
+                Text(
+                  'if you like it, also you can donate with what you think es fair.\n\nThanks and good mining!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    wordSpacing: 5,
+                    height: 2,
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GradientButton(
-                    child: Text(
-                      'Ether Wallet',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GradientButton(
+                      child: Text(
+                        'Ether Wallet',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
+                      height: 60,
+                      isButton: true,
+                      width: 150,
+                      onPressed: () {
+                        FlutterClipboard.copy('0xF02bB51E0aEbCE3FeDd890555A19582FEBa1Eb3a')
+                            .then((value) => Scaffold.of(context).showSnackBar(snackBar));
+                      },
                     ),
-                    height: 60,
-                    isButton: true,
-                    width: 150,
-                    onPressed: () {
-                      FlutterClipboard.copy('0xF02bB51E0aEbCE3FeDd890555A19582FEBa1Eb3a')
-                          .then((value) => Scaffold.of(context).showSnackBar(snackBar));
-                    },
-                  ),
-                  SizedBox(width: 20),
-                  GradientButton(
-                    child: Text(
-                      'Bitcon Wallet',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+                    SizedBox(width: 20),
+                    GradientButton(
+                      child: Text(
+                        'Bitcon Wallet',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
+                      height: 60,
+                      isButton: true,
+                      width: 150,
+                      onPressed: () {
+                        FlutterClipboard.copy('1BrWnbsMR9fzPAvzcvmy6h15iMZGPtpsD3')
+                            .then((value) => Scaffold.of(context).showSnackBar(snackBar));
+                      },
                     ),
-                    height: 60,
-                    isButton: true,
-                    width: 150,
-                    onPressed: () {
-                      FlutterClipboard.copy('1BrWnbsMR9fzPAvzcvmy6h15iMZGPtpsD3')
-                          .then((value) => Scaffold.of(context).showSnackBar(snackBar));
-                    },
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
